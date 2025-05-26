f8a60988e1a36ba1e5baee23644002029d2822eb
Alina722 <qianyaotzz@gmail.com>
2025-05-26 15:46:50 +0800

    倒數計時器 碼錶功能一次更新

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -39,11 +39,29 @@
             this.txtDate = new System.Windows.Forms.TextBox();
             this.txtTime = new System.Windows.Forms.TextBox();
             this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnPause = new System.Windows.Forms.Button();
+            this.btnLog = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.listStopWatchLog = new System.Windows.Forms.ListBox();
+            this.txtStopWatch = new System.Windows.Forms.TextBox();
             this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.btnCountStop = new System.Windows.Forms.Button();
+            this.btnCountPause = new System.Windows.Forms.Button();
+            this.btnCountStart = new System.Windows.Forms.Button();
+            this.cmbCountSecond = new System.Windows.Forms.ComboBox();
+            this.cmbCountMin = new System.Windows.Forms.ComboBox();
+            this.cmbCountHour = new System.Windows.Forms.ComboBox();
+            this.txtCountDown = new System.Windows.Forms.TextBox();
             this.timerClock = new System.Windows.Forms.Timer(this.components);
             this.timerAlert = new System.Windows.Forms.Timer(this.components);
+            this.timerStopWatch = new System.Windows.Forms.Timer(this.components);
+            this.timerCountDown = new System.Windows.Forms.Timer(this.components);
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.tabPage3.SuspendLayout();
             this.SuspendLayout();
             // 
             // tabControl1
@@ -144,6 +162,13 @@
             // 
             // tabPage2
             // 
+            this.tabPage2.Controls.Add(this.btnStop);
+            this.tabPage2.Controls.Add(this.btnPause);
+            this.tabPage2.Controls.Add(this.btnLog);
+            this.tabPage2.Controls.Add(this.btnReset);
+            this.tabPage2.Controls.Add(this.btnStart);
+            this.tabPage2.Controls.Add(this.listStopWatchLog);
+            this.tabPage2.Controls.Add(this.txtStopWatch);
             this.tabPage2.Location = new System.Drawing.Point(4, 25);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
@@ -152,14 +177,163 @@
             this.tabPage2.Text = "碼表";
             this.tabPage2.UseVisualStyleBackColor = true;
             // 
+            // btnStop
+            // 
+            this.btnStop.BackColor = System.Drawing.Color.Pink;
+            this.btnStop.Font = new System.Drawing.Font("微軟正黑體", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnStop.Location = new System.Drawing.Point(406, 288);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(305, 47);
+            this.btnStop.TabIndex = 6;
+            this.btnStop.Text = "停止並歸零";
+            this.btnStop.UseVisualStyleBackColor = false;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnPause
+            // 
+            this.btnPause.Font = new System.Drawing.Font("微軟正黑體", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnPause.Location = new System.Drawing.Point(570, 150);
+            this.btnPause.Name = "btnPause";
+            this.btnPause.Size = new System.Drawing.Size(141, 47);
+            this.btnPause.TabIndex = 5;
+            this.btnPause.Text = "暫停";
+            this.btnPause.UseVisualStyleBackColor = true;
+            this.btnPause.Click += new System.EventHandler(this.btnPause_Click);
+            // 
+            // btnLog
+            // 
+            this.btnLog.Font = new System.Drawing.Font("微軟正黑體", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnLog.Location = new System.Drawing.Point(570, 223);
+            this.btnLog.Name = "btnLog";
+            this.btnLog.Size = new System.Drawing.Size(141, 47);
+            this.btnLog.TabIndex = 4;
+            this.btnLog.Text = "記錄";
+            this.btnLog.UseVisualStyleBackColor = true;
+            this.btnLog.Click += new System.EventHandler(this.btnLog_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Font = new System.Drawing.Font("微軟正黑體", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnReset.Location = new System.Drawing.Point(406, 223);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(143, 47);
+            this.btnReset.TabIndex = 3;
+            this.btnReset.Text = "歸零";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("微軟正黑體", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnStart.Location = new System.Drawing.Point(406, 150);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(143, 47);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "開始";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // listStopWatchLog
+            // 
+            this.listStopWatchLog.FormattingEnabled = true;
+            this.listStopWatchLog.ItemHeight = 15;
+            this.listStopWatchLog.Location = new System.Drawing.Point(26, 114);
+            this.listStopWatchLog.Name = "listStopWatchLog";
+            this.listStopWatchLog.Size = new System.Drawing.Size(311, 289);
+            this.listStopWatchLog.TabIndex = 1;
+            // 
+            // txtStopWatch
+            // 
+            this.txtStopWatch.Font = new System.Drawing.Font("微軟正黑體", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.txtStopWatch.Location = new System.Drawing.Point(26, 21);
+            this.txtStopWatch.Name = "txtStopWatch";
+            this.txtStopWatch.Size = new System.Drawing.Size(738, 87);
+            this.txtStopWatch.TabIndex = 0;
+            this.txtStopWatch.Text = "按下開始按鍵計時";
+            this.txtStopWatch.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
             // tabPage3
             // 
+            this.tabPage3.Controls.Add(this.btnCountStop);
+            this.tabPage3.Controls.Add(this.btnCountPause);
+            this.tabPage3.Controls.Add(this.btnCountStart);
+            this.tabPage3.Controls.Add(this.cmbCountSecond);
+            this.tabPage3.Controls.Add(this.cmbCountMin);
+            this.tabPage3.Controls.Add(this.cmbCountHour);
+            this.tabPage3.Controls.Add(this.txtCountDown);
             this.tabPage3.Location = new System.Drawing.Point(4, 25);
             this.tabPage3.Name = "tabPage3";
             this.tabPage3.Size = new System.Drawing.Size(786, 420);
             this.tabPage3.TabIndex = 2;
             this.tabPage3.Text = "倒數";
             this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // btnCountStop
+            // 
+            this.btnCountStop.BackColor = System.Drawing.Color.Tomato;
+            this.btnCountStop.Font = new System.Drawing.Font("新細明體", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnCountStop.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnCountStop.Location = new System.Drawing.Point(37, 233);
+            this.btnCountStop.Name = "btnCountStop";
+            this.btnCountStop.Size = new System.Drawing.Size(375, 42);
+            this.btnCountStop.TabIndex = 6;
+            this.btnCountStop.Text = "停止";
+            this.btnCountStop.UseVisualStyleBackColor = false;
+            this.btnCountStop.Click += new System.EventHandler(this.btnCountStop_Click);
+            // 
+            // btnCountPause
+            // 
+            this.btnCountPause.Location = new System.Drawing.Point(37, 185);
+            this.btnCountPause.Name = "btnCountPause";
+            this.btnCountPause.Size = new System.Drawing.Size(375, 42);
+            this.btnCountPause.TabIndex = 5;
+            this.btnCountPause.Text = "暫停";
+            this.btnCountPause.UseVisualStyleBackColor = true;
+            this.btnCountPause.Click += new System.EventHandler(this.btnCountPause_Click);
+            // 
+            // btnCountStart
+            // 
+            this.btnCountStart.Location = new System.Drawing.Point(37, 137);
+            this.btnCountStart.Name = "btnCountStart";
+            this.btnCountStart.Size = new System.Drawing.Size(375, 42);
+            this.btnCountStart.TabIndex = 4;
+            this.btnCountStart.Text = "開始倒數";
+            this.btnCountStart.UseVisualStyleBackColor = true;
+            this.btnCountStart.Click += new System.EventHandler(this.btnCountStart_Click);
+            // 
+            // cmbCountSecond
+            // 
+            this.cmbCountSecond.FormattingEnabled = true;
+            this.cmbCountSecond.Location = new System.Drawing.Point(291, 108);
+            this.cmbCountSecond.Name = "cmbCountSecond";
+            this.cmbCountSecond.Size = new System.Drawing.Size(121, 23);
+            this.cmbCountSecond.TabIndex = 3;
+            // 
+            // cmbCountMin
+            // 
+            this.cmbCountMin.FormattingEnabled = true;
+            this.cmbCountMin.Location = new System.Drawing.Point(164, 108);
+            this.cmbCountMin.Name = "cmbCountMin";
+            this.cmbCountMin.Size = new System.Drawing.Size(121, 23);
+            this.cmbCountMin.TabIndex = 2;
+            // 
+            // cmbCountHour
+            // 
+            this.cmbCountHour.FormattingEnabled = true;
+            this.cmbCountHour.Location = new System.Drawing.Point(37, 108);
+            this.cmbCountHour.Name = "cmbCountHour";
+            this.cmbCountHour.Size = new System.Drawing.Size(121, 23);
+            this.cmbCountHour.TabIndex = 1;
+            // 
+            // txtCountDown
+            // 
+            this.txtCountDown.Font = new System.Drawing.Font("微軟正黑體", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.txtCountDown.Location = new System.Drawing.Point(37, 15);
+            this.txtCountDown.Name = "txtCountDown";
+            this.txtCountDown.Size = new System.Drawing.Size(717, 87);
+            this.txtCountDown.TabIndex = 0;
+            this.txtCountDown.Text = "00:00:00:000";
+            this.txtCountDown.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // timerClock
             // 
@@ -169,6 +343,16 @@
             // 
             this.timerAlert.Interval = 1000;
             this.timerAlert.Tick += new System.EventHandler(this.timerAlert_Tick);
+            // 
+            // timerStopWatch
+            // 
+            this.timerStopWatch.Interval = 1;
+            this.timerStopWatch.Tick += new System.EventHandler(this.timerStopWatch_Tick);
+            // 
+            // timerCountDown
+            // 
+            this.timerCountDown.Interval = 1;
+            this.timerCountDown.Tick += new System.EventHandler(this.timerCountDown_Tick);
             // 
             // Form1
             // 
@@ -181,6 +365,10 @@
             this.tabControl1.ResumeLayout(false);
             this.tabPage1.ResumeLayout(false);
             this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.tabPage3.ResumeLayout(false);
+            this.tabPage3.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -200,6 +388,22 @@
         private System.Windows.Forms.TextBox txtWeekDay;
         private System.Windows.Forms.Timer timerClock;
         private System.Windows.Forms.Timer timerAlert;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.ListBox listStopWatchLog;
+        private System.Windows.Forms.TextBox txtStopWatch;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnPause;
+        private System.Windows.Forms.Button btnLog;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Timer timerStopWatch;
+        private System.Windows.Forms.Button btnCountStart;
+        private System.Windows.Forms.ComboBox cmbCountSecond;
+        private System.Windows.Forms.ComboBox cmbCountMin;
+        private System.Windows.Forms.ComboBox cmbCountHour;
+        private System.Windows.Forms.TextBox txtCountDown;
+        private System.Windows.Forms.Button btnCountStop;
+        private System.Windows.Forms.Button btnCountPause;
+        private System.Windows.Forms.Timer timerCountDown;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using NAudio.Wave;           // 音效檔播放器函式庫
 using System.IO;
+using System.Diagnostics;
 
 namespace SimpleClock
 {
@@ -38,6 +39,27 @@ namespace SimpleClock
             for (int i = 0; i <= 59; i++)
                 cmbMin.Items.Add(string.Format("{0:00}", i));
             cmbMin.SelectedIndex = 0;
+
+            // 設定小時下拉選單的選單內容，建立小時的清單，數字範圍為00-23
+            for (int i = 0; i <= 23; i++)
+            {
+                cmbHour.Items.Add(string.Format("{0:00}", i));
+                cmbCountHour.Items.Add(string.Format("{0:00}", i));
+            }
+
+            // 設定分鐘下拉選單的選單內容，建立分鐘的清單，數字範圍為00-59
+            for (int i = 0; i <= 59; i++)
+            {
+                cmbMin.Items.Add(string.Format("{0:00}", i));
+                cmbCountMin.Items.Add(string.Format("{0:00}", i));
+                cmbCountSecond.Items.Add(string.Format("{0:00}", i));
+            }
+
+            cmbHour.SelectedIndex = 0;
+            cmbMin.SelectedIndex = 0;
+            cmbCountHour.SelectedIndex = 0;
+            cmbCountMin.SelectedIndex = 0;
+            cmbCountSecond.SelectedIndex = 0;
         }
 
         private void timerClock_Tick(object sender, EventArgs e)
@@ -105,6 +127,149 @@ namespace SimpleClock
             timerAlert.Stop(); // 停止鬧鐘計時器
             btnSetAlert.Enabled = true;
             btnCancelAlert.Enabled = false;
+        }
+
+        List<string> StopWatchLog = new List<string>();         // 碼表紀錄清單 
+        Stopwatch sw = new Stopwatch();                         // 宣告一個碼表物件
+
+        // timerStopWatch_tick：每毫秒執行一次，所以更新的速度會比較快
+        private void timerStopWatch_Tick(object sender, EventArgs e)
+        {
+            txtStopWatch.Text = sw.Elapsed.ToString("hh':'mm':'ss':'fff");    // 顯示碼表時間
+        }
+
+        // 啟動碼表
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            sw.Start();             // 啟動碼表
+            timerStopWatch.Start(); // 開始讓碼表文字顯示
+        }
+
+        // 停止並歸零碼表
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            sw.Reset();                           // 停止並歸零碼表
+            timerStopWatch.Stop();                // 停止讓碼表文字顯示     
+            txtStopWatch.Text = "00:00:00:000";   // 讓碼表文字「歸零」
+            listStopWatchLog.Items.Clear();       // 清空 ListBox 中的元素
+            StopWatchLog.Clear();                 // 清除暫存碼表紀錄清單
+        }
+
+        // 歸零按鍵會判斷你是否先按下暫停？來決定是否記錄碼表時間
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            // 如果碼表還在跑，就紀錄目前的時間，最後歸零再啟動碼錶
+            if (sw.IsRunning)
+            {
+                logRecord();
+                sw.Restart(); // 歸零碼表，碼表仍繼續進行  
+            }
+            else
+            {
+                sw.Reset(); // 如果碼表沒在跑，停止並歸零碼表
+                txtStopWatch.Text = "00:00:00:000";   // 讓碼表文字「歸零」
+            }
+        }
+
+        // 停止碼表
+        private void btnPause_Click(object sender, EventArgs e)
+        {
+            sw.Stop();                  // 停止碼表，但不歸零
+            timerStopWatch.Stop();      // 停止讓碼表文字顯示  
+        }
+
+        // 碼表時間紀錄
+        private void btnLog_Click(object sender, EventArgs e)
+        {
+            logRecord();
+        }
+
+        // 碼表時間紀錄
+        private void logRecord()
+        {
+            listStopWatchLog.Items.Clear(); // 清空 ListBox 中的元素
+            StopWatchLog.Add(txtStopWatch.Text); // 將碼表時間增加到暫存碼表紀錄清單裡
+
+            // 依照碼表紀錄清單「依照最新時間順序」顯示
+            int i = StopWatchLog.Count;
+            while (i > 0)
+            {
+                listStopWatchLog.Items.Add(String.Format("第 {0} 筆紀錄：{1}", i.ToString(), StopWatchLog[i - 1] + "\n"));
+                i--;
+            }
+        }
+
+        bool isCountDownReset = true;                           // 用來紀錄是不是重新設定
+        TimeSpan ts;                                            // 宣告一個時間間隔變數
+
+
+        // timerCountDown_tick：每一秒執行一次
+        private void timerCountDown_Tick(object sender, EventArgs e)
+        {
+            txtCountDown.Text = ts.ToString("hh':'mm':'ss");    // 顯示時間
+            ts = ts.Subtract(TimeSpan.FromSeconds(1));          // 每一秒鐘將顯示時間減掉一秒
+
+            if (txtCountDown.Text == "00:00:00")
+            {
+                try
+                {
+                    stopWaveOut();
+
+                    // 指定聲音檔的相對路徑，可以使用MP3
+                    string audioFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Resources", "alert.wav");
+
+                    // 使用 AudioFileReader 來讀取聲音檔
+                    audioFileReader = new AudioFileReader(audioFilePath);
+
+                    // 初始化 WaveOutEvent
+                    waveOut = new WaveOutEvent();
+                    waveOut.Init(audioFileReader);
+
+                    // 播放聲音檔
+                    waveOut.Play();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("無法播放聲音檔，錯誤資訊: " + ex.Message);
+                }
+                finally
+                {
+                    timerCountDown.Stop();         // 停止鬧鐘計時器
+                }
+            }
+        }
+
+        // timerCountDown_tick：每一秒執行一次
+        private void btnCountStart_Click(object sender, EventArgs e)
+        {
+            // 進行判斷，判斷是不是有按過停止計時器按鍵
+            if (isCountDownReset == true)
+            {
+                int Hour = int.Parse(cmbCountHour.SelectedItem.ToString());
+                int Min = int.Parse(cmbCountMin.SelectedItem.ToString());
+                int Sec = int.Parse(cmbCountSecond.SelectedItem.ToString());
+                ts = new TimeSpan(Hour, Min, Sec); // 設定倒數時間
+            }
+            isCountDownReset = false;
+            timerCountDown.Start();
+        }
+
+        // 暫停倒數計時器按鍵
+        private void btnCountPause_Click(object sender, EventArgs e)
+        {
+            timerCountDown.Stop();
+        }
+
+        // 停止計時器按鍵
+        private void btnCountStop_Click(object sender, EventArgs e)
+        {
+            stopWaveOut(); // 關閉鬧鐘聲音
+            isCountDownReset = true;
+            timerCountDown.Stop();
+            txtCountDown.Text = "00:00:00";
+            cmbCountHour.SelectedIndex = 0;
+            cmbCountMin.SelectedIndex = 0;
+            cmbCountSecond.SelectedIndex = 0;
         }
     }
 }
